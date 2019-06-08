e562e2bf7fb175c9d6692650a885f355db9a902e
aben20807 <aben20807@gmail.com>
2019-06-08 23:57:29 +0800

    Create resources dir for db file

--- src/db/sqlite/mod.rs
+++ src/db/sqlite/mod.rs
@@ -3,7 +3,8 @@ use rusqlite::{params, Connection, Result};
 mod init;
 
 pub fn connect() -> rusqlite::Connection {
-    let conn = Connection::open("./src/db/sqlite/mydb.sqlite").unwrap();
+    let _ = std::fs::create_dir_all("./resources/");
+    let conn = Connection::open("./resources/mydb.sqlite").unwrap();
     conn
 }
 
@@ -28,41 +29,3 @@ pub fn drop_db(conn: &rusqlite::Connection) {
 pub fn exec_sql<'a>(conn: &'a rusqlite::Connection, command: &str) -> Result<rusqlite::Statement<'a>> {
     conn.prepare(command)
 }
-
-// pub fn test() -> Result<()> {
-//     // let conn = Connection::open_in_memory()?;
-//     let conn = Connection::open("./src/db/sqlite/mydb.sqlite")?;
-
-//     conn.execute(
-//         "CREATE TABLE person (
-//                   id              INTEGER PRIMARY KEY,
-//                   name            TEXT NOT NULL,
-//                   data            BLOB
-//                   )",
-//         params![],
-//     )?;
-//     let me = Person {
-//         id: 0,
-//         name: "Steven".to_string(),
-//         data: None,
-//     };
-//     conn.execute(
-//         "INSERT INTO person (name, data)
-//                   VALUES (?1, ?2)",
-//         params![me.name, me.data],
-//     )?;
-
-//     let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-//     let person_iter = stmt.query_map(params![], |row| {
-//         Ok(Person {
-//             id: row.get(0)?,
-//             name: row.get(1)?,
-//             data: row.get(2)?,
-//         })
-//     })?;
-
-//     for person in person_iter {
-//         println!("Found person {:?}", person.unwrap());
-//     }
-//     Ok(())
-// }
